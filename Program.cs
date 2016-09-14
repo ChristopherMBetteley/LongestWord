86254ee0c5d13bb0207c5401d394762791f43096
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-09-13 21:34:49 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentenceOriginal, longestWord = "";
+            int LengthTest = 0;
+            string[] individualWords;
+
+            Console.WriteLine("*****************************");
+            Console.WriteLine("          Welcome");
+            Console.WriteLine("*****************************");
+
+            while (true)
+            {
+                
+                Console.WriteLine("\n\nPlease provide a sentence.");
+                sentenceOriginal = Console.ReadLine();
+
+                individualWords = sentenceOriginal.Split(' ');
+
+                int i = 0; //itteration and index counter
+                foreach (string words in individualWords)
+                {
+                    if (words.Length > LengthTest)
+                    {
+                        LengthTest = words.Length;
+                        longestWord = individualWords[i];
+                    }
+
+                    i++;
+                }
+                Console.WriteLine(longestWord + " is the longest word and is " + LengthTest + " characters long");
+
+                Console.ReadKey();
+            }
+            
+            
+        }
+    }
+}
